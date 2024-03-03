7b195dba2d1ac0f59c7431e65463e33808609332
Soham Sinha <sohamsinha299@gmail.com>
2024-03-04 00:07:06 +0530

    .

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-plt.subplot(1,2,1)
-strat_train_set["Survived"].hist()
-strat_train_set["Pclass"].hist()
-
-plt.subplot(1,2,2)
-strat_test_set["Survived"].hist()
-strat_test_set["Pclass"].hist()
-plt.show()
